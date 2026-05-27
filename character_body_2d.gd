365c591da6b122135254674633102e1f1ce7ec71
KOZUSEGI <ovszell@gmail.com>
2026-05-27 20:08:28 +0700

    Added more file

---
+++ character_body_2d.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var speed = 200
+var direction = Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1 
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1 	
+	direction = direction.normalized()
+	velocity = direction*speed
+	move_and_slide()
